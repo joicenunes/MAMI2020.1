b34e45b8f8ebfbeb3392fa7afdbd316b9d4f0177
joicenunes <anunes.joice@gmail.com>
2020-03-13 21:11:35 -0300

    adding pages

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+let topnav = document.getElementById('topnav');
+let sticky = topnav.offsetTop;
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add('sticky');
+  } else {
+    topnav.classList.remove('sticky');
+  }
+};
+
+let accordions = document.getElementsByClassName('accordion');
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+    let panel = this.nextElementSibling;
+    if (panel.style.display == 'block')
+      panel.style.display = 'none';
+    else
+      panel.style.display = 'block';
+    });
+}
